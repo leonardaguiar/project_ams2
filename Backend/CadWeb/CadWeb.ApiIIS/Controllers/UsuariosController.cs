edb9af6e526c32af976025b05005a761a6371dae
Leonardo Aguiar <leo.aguiar.silva@gmail.com>
2017-07-14 19:10:19 -0300

    Implementação da Api com o IIS
    A api como self-host esta gerando alguns problemas em sua publicação. O deploy via aws com suporte a .Net e IIS vai facilitar o andamento do projeto.

---
+++ Backend/CadWeb/CadWeb.ApiIIS/Controllers/UsuariosController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CadWeb.Dados.DataContexts;
+using CadWeb.Dominio;
+
+namespace CadWeb.ApiIIS.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class UsuariosController : ApiController
+    {
+        private CadWebDataContext db = new CadWebDataContext();
+            [Route("usuarios")]
+            public HttpResponseMessage GetUsuarios()
+            {
+
+                var result = db.Usuarios.ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            [Route("usuarios/{id}")]
+            public HttpResponseMessage GetUsuariosById(int id)
+            {
+
+                var result = db.Usuarios.Where(x => x.Id == id).ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+
+            [Route("usuarios/{nome}/{senha}")]
+            public HttpResponseMessage GetUsuariosByNome(string nome, string senha)
+            {
+
+                var result = db.Usuarios.Where(x => x.Nome == nome && x.Senha == senha).ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+
+            [HttpPost]
+            [Route("usuarios")]
+            public HttpResponseMessage PosUsuario(Usuario usuario)
+            {
+                if (usuario == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest);
+                }
+
+                try
+                {
+                    db.Usuarios.Add(usuario);
+                    db.SaveChanges();
+                    var result = usuario;
+                    return Request.CreateResponse(HttpStatusCode.Created, result);
+
+                }
+                catch
+                {
+
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Fallha na inserção do usuario");
+                }
+
+            }
+
+            [Route("teste")]
+            public HttpResponseMessage GetUsuariosTeste(string teste)
+            {
+
+                var result = new
+                {
+                    hora = 17,
+                    minuto = 12,
+                    segundo = 25
+                };
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return db.Usuarios.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Backend/CadWeb/CadWeb.ApiIIS/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CadWeb.ApiIIS.WebApiApplication" Language="C#" %>
